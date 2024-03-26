8852bc95945cd5ac60c84d86d2ea2c572015c646
watsuyo <watsuyo.dev@gmail.com>
2024-03-26 14:26:38 +0100

    Update code formatting and fix bug in file processing

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import path, { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [react(), dts({ include: ["src", "lib"] })],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/lib/main.ts"),
+      formats: ["es"],
+    },
+    copyPublicDir: false,
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime"],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
