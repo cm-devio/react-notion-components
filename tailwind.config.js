46daa6201189af09b68d8bf730ceef3cd7495efe
watsuyo <watsuyo.dev@gmail.com>
2024-09-03 14:05:10 +0200

    refactor: Remove unused SVG files, CSS styles, and update npm dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 import tailwindcssAnimate from "tailwindcss-animatecss";
-export const darkMode = ["class"];
 export const content = ["./src/**/*.{ts,tsx}"];
-export const prefix = "";
 export const theme = {
-	container: {
-		center: true,
-		padding: "2rem",
-		screens: {
-			"2xl": "1400px",
-		},
-	},
 	extend: {
-		borderRadius: {
-			lg: "var(--radius)",
-			md: "calc(var(--radius) - 2px)",
-			sm: "calc(var(--radius) - 4px)",
-		},
-		keyframes: {
-			"accordion-down": {
-				from: { height: "0" },
-				to: { height: "var(--radix-accordion-content-height)" },
-			},
-			"accordion-up": {
-				from: { height: "var(--radix-accordion-content-height)" },
-				to: { height: "0" },
-			},
-		},
-		animation: {
-			"accordion-down": "accordion-down 0.2s ease-out",
-			"accordion-up": "accordion-up 0.2s ease-out",
+		backgroundImage: {
+			"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+			"gradient-conic":
+				"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
 		},
 	},
 };
